a6964de095ec712c5757af21118cf3386aebded4
Youngju KIM <fjvbn2003@gmail.com>
2019-01-13 04:12:51 +0900

    #40 Finishing this marioplan application (react & firebase exercise)

---
+++ dist/precache-manifest.8671bcaa9ee2e04251c401720cbc5fe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e1b29cee23c6cd0c34b",
+    "url": "/static/js/main.6e1b29ce.chunk.js"
+  },
+  {
+    "revision": "9f68ae3ba583cba57bb1",
+    "url": "/static/js/1.9f68ae3b.chunk.js"
+  },
+  {
+    "revision": "6e1b29cee23c6cd0c34b",
+    "url": "/static/css/main.322fec1a.chunk.css"
+  },
+  {
+    "revision": "52a45ba0088403119332d084917ab06b",
+    "url": "/index.html"
+  }
+];
