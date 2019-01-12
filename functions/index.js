b4607b137324fe957ae2c035f660e0cedc018f5c
Youngju KIM <fjvbn2003@gmail.com>
2019-01-13 02:57:04 +0900

    #37 Cloud Function Auth Trigger

--- functions/index.js
+++ functions/index.js
@@ -27,3 +27,18 @@ exports.projectCreated = functions.firestore
 
     });
 
+exports.userJoined =  functions.auth.user()
+    .onCreate(user =>{
+        return admin.firestore().collection('users')
+        .doc(user.uid).get().then( doc =>{
+            const newUser = doc.data();
+            const notification = {
+                content: 'Joined the party',
+                user: `${newUser.firstName} ${newUser.lastName}`,
+                time: admin.firestore.FieldValue.serverTimestamp()
+        }
+        return createNotification(notification);
+
+    })
+    
+})
